6e8a9c630090a301c80097ea0055aab90be2decf
Mirza Brunjadze <brunjadzem@gmail.com>
2018-03-08 19:00:05 +0400

    use namespace instead of prefix

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,49 @@
-declare class Adonis$Env {
-  constructor(appRoot: string);
-  load(filePath: string, overwrite: boolean, encoding: string): void;
-  getEnvPath(): string;
-  get(key: string, defaultValue: any): any;
-  set(key: string, value: any): void;
+declare namespace Adonis {
+  class Env {
+    constructor(appRoot: string)
+    load(filePath: string, overwrite: boolean, encoding: string): void
+    getEnvPath(): string
+    get(key: string, defaultValue: any): any
+    set(key: string, value: any): void
+  }
+
+  class Config {
+    constructor(configPath: string)
+    syncWithFileSystem(): void
+    get(key: string, defaultValue: any): any
+    merge(key: string, defaultValues: Object, customizer?: Function): Object
+    set(key: string, value: any): void
+  }
+
+  class Route {}
+  class BriskRoute {}
+  class RouteGroup {}
+  class RouteResource {}
+  class RouteManager {
+    route(
+      route: string,
+      handler: Function | string,
+      verbs: Array<string>
+    ): Route
+    get(route: string, handler: Function | string): Route
+    post(route: string, handler: Function | string): Route
+    put(route: string, handler: Function | string): Route
+    patch(route: string, handler: Function | string): Route
+    delete(route: string, handler: Function | string): Route
+    any(route: string, handler: Function | string): Route
+    on(route: string): BriskRoute
+    match(url: string, verb: string, host?: string): Object | null
+    group(name: string, callback: Function): RouteGroup
+    resource(resouce: string, controller: string): RouteResource
+    list(): Array<any>
+    url(
+      routeNameOrHandler: string,
+      data?: Object,
+      options?: string
+    ): string | null
+  }
 }
 
-declare class Adonis$Config {
-  constructor(configPath: string);
-  syncWithFileSystem(): void;
-  get(key: string, defaultValue: any): any;
-  merge(key: string, defaultValues: Object, customizer?: Function): Object;
-  set(key: string, value: any): void;
-}
-
-declare class Adonis$Route {}
-declare class Adonis$BriskRoute {}
-declare class Adonis$RouteGroup {}
-declare class Adonis$RouteResource {}
-declare class Adonis$RouteManager {
-  route(
-    route: string,
-    handler: Function | string,
-    verbs: Array<string>
-  ): Adonis$Route;
-  get(route: string, handler: Function | string): Adonis$Route;
-  post(route: string, handler: Function | string): Adonis$Route;
-  put(route: string, handler: Function | string): Adonis$Route;
-  patch(route: string, handler: Function | string): Adonis$Route;
-  delete(route: string, handler: Function | string): Adonis$Route;
-  any(route: string, handler: Function | string): Adonis$Route;
-  on(route: string): Adonis$BriskRoute;
-  match(url: string, verb: string, host?: string): Object | null;
-  group(name: string, callback: Function): Adonis$RouteGroup;
-  resource(resouce: string, controller: string): Adonis$RouteResource;
-  list(): Array<any>;
-  url(
-    routeNameOrHandler: string,
-    data?: Object,
-    options?: string
-  ): string | null;
-}
-
-declare function use(path: "Env"): Adonis$Env;
-declare function use(path: "Config"): Adonis$Config;
-declare function use(path: "Route"): Adonis$RouteManager;
+declare function use(path: 'Env'): Adonis.Env
+declare function use(path: 'Config'): Adonis.Config
+declare function use(path: 'Route'): Adonis.RouteManager
